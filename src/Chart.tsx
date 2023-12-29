256eb04a7e51b8154e63d009b92d393af19abf83
Dipam Sen <funplanet.m4f@gmail.com>
2023-12-29 19:54:30 +0530

    Update viewport meta tag and chart dimensions

--- src/Chart.tsx
+++ src/Chart.tsx
@@ -22,17 +22,14 @@ const initials = (txt: string) => {
 };
 
 export default function Chart({ data }: any) {
+  const windowWidth = window.innerWidth;
+  const w = windowWidth > 1000 ? 1000 : windowWidth;
+
   return (
     <BarChart
-      width={1080}
-      height={450}
+      width={w}
+      height={w * 0.6}
       data={data}
-      margin={{
-        top: 5,
-        right: 30,
-        left: 20,
-        bottom: 5,
-      }}
     >
       <CartesianGrid strokeDasharray="3 3" />
       <XAxis dataKey="name" allowDataOverflow={true} allowReorder="yes" tickFormatter={(tick) => initials(tick)} />
@@ -41,11 +38,11 @@ export default function Chart({ data }: any) {
       <Bar
         dataKey={"value"}
         barSize={25}
-        label={(x) => (
+        label={windowWidth > 800 && ((x) => (
           <text fill="#fff" transform={`translate(${x.x + x.width / 2 + 5}, ${x.y + x.height - 5}) rotate(-90)`}>
             {x.name}
           </text>
-        )}
+        ))}
         fill="#A87A81"
       />
     </BarChart>
